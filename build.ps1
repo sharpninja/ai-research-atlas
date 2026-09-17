a0f36e130e311491176fa0eb61a2959ba84749c2
Sharp Ninja <ninja@thesharp.ninja>
2026-09-17 11:09:31 -0500

    docs: detail AI scientific benefits

--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop'
 $siteRoot = $PSScriptRoot
-$releaseVersion = '24'
-$publishedAtUtc = '2026-09-17T16:00:03Z'
+$releaseVersion = '25'
+$publishedAtUtc = '2026-09-17T16:05:55Z'
 $publishDateLabel = [datetimeoffset]::Parse($publishedAtUtc, [Globalization.CultureInfo]::InvariantCulture).ToUniversalTime().ToString("d MMMM yyyy, HH:mm:ss 'UTC'", [Globalization.CultureInfo]::InvariantCulture)
 $entries = (Import-PowerShellDataFile (Join-Path $siteRoot 'research.psd1')).Entries
 $bibliographyIndex = Get-Content (Join-Path $siteRoot 'bibliography-index.json') -Raw -Encoding UTF8 | ConvertFrom-Json
